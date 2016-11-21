0f556548a54b1ade433503f3804953b8df6d9f28
Jakub Jodelka <jakub@jodelka.me>
2016-11-21 16:33:58 +0100

    [rx-tests] add: powerMock flavour tests

---
+++ rxsample/src/test-power-mock/java/com/mateuszkoslacz/moviper/rxsample/viper/presenter/ListingPresenterTest.java
@@ -0,0 +1,14 @@
+package com.mateuszkoslacz.moviper.rxsample.viper.presenter;
+
+
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * Created by mateuszkoslacz on 17.11.2016.
+ */
+
+@RunWith(PowerMockRunner.class)
+public class ListingPresenterTest {
+
+}
